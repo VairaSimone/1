f79c22bd78a6e8943a4b69411b7e235072b47549
Simone <158765247+VairaSimone@users.noreply.github.com>
2026-09-17 22:50:14 +0200

    fix: close behavioral feedback loops and resource failures

--- asami-backend/src/server.js
+++ asami-backend/src/server.js
@@ -9,6 +9,7 @@ require("./services/behavioral-integrity-bootstrap").install();
 require("./services/development-duration-bootstrap").install();
 require("./services/decision-sql-compat-bootstrap").install();
 require("./services/action-runtime-bootstrap").install();
+require("./services/behavior-fix-bootstrap").install();
 const { env } = require("./config/env");
 const logger = require("./lib/logger");
 const { ensureDatabase } = require("./db/database-init");
